47eb4078c1607ad639976479c7761b8792203815
Louis Gevers <louis.gevers@gmail.com>
2020-03-18 14:04:36 +0100

    Create Spring Boot application

---
+++ server/src/main/java/io/louisgevers/coronapartyserver/Application.java
@@ -0,0 +1,16 @@
+package io.louisgevers.coronapartyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class to start the Spring Boot server application.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ server/src/test/java/io/louisgevers/coronapartyserver/ApplicationTest.java
@@ -0,0 +1,14 @@
+package io.louisgevers.coronapartyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
