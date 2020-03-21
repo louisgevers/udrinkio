9e4a334709409a965d8c3239cfe7d960ce8e0a50
Louis Gevers <louis.gevers@gmail.com>
2020-03-21 17:38:58 +0100

    Create Cover and GamesPanel components

---
+++ webapp/src/component/Cover/Cover.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import "./Cover.css"
+
+class Cover extends Component {
+    render() {
+        return (
+            <div className="Cover">
+                <p>[COVER]</p>
+            </div>
+        )
+    }
+}
+
+export default Cover
---
+++ webapp/src/component/GamesPanel/GamesPanel.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import "./GamesPanel.css"
+
+class GamesPanel extends Component {
+    render() {
+        return (
+            <div className="GamesPanel">
+                <p>[GAMESPANEL]</p>
+            </div>
+        )
+    }
+}
+
+export default GamesPanel
