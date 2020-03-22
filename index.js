fbbf3cdc8cec64bf9bc1d4b96de7f0c60fc447be
Louis Gevers <louis.gevers@gmail.com>
2020-03-22 22:28:53 +0100

    Fix server origins

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-// var server = require('http').createServer(app)
+const http = require('http').createServer(app)
+var io = require('socket.io').listen(http)
 
 
 app.use(express.static(path.join(__dirname, 'build')))
@@ -10,15 +11,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-const port = process.env.PORT || 8080;
-server = app.listen(port, () => {
-  console.log(`Listening on port ${ port }`)
-});
-
-var io = require('socket.io').listen(server)
-
-io.set('origins', 'http://localhost.com:8080');
-
 io.on('connection', (socket) => {
   console.log('a user connected!')
 })
+
+const port = process.env.PORT || 8080;
+http.listen(port, () => {
+  console.log(`Listening on port ${ port }`)
+});
