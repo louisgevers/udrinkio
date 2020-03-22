4c06e67f7ba92b97afc7a9f3667bb6f0225723af
Louis Gevers <louis.gevers@gmail.com>
2020-03-22 12:10:49 +0100

    Create GamesPanel components and styles

--- webapp/src/component/GamesPanel/GamesPanel.js
+++ webapp/src/component/GamesPanel/GamesPanel.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react"
 import "./GamesPanel.css"
 
+import GameTile from "./GameTile/GameTile"
+
+import games from "../../data/games.json"
+
 class GamesPanel extends Component {
     render() {
         return (
             <div className="GamesPanel">
-                <p>[GAMESPANEL]</p>
+                {games.map(game => {
+                    return <GameTile key={game.name} game={game} />
+                })}
             </div>
         )
     }
