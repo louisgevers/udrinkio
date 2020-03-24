b22624e956dc1f3e41492f86bf8655c185ec6e53
Louis Gevers <louis.gevers@gmail.com>
2020-03-24 09:58:37 +0100

    Create Chat and Lobby components for Game page

---
+++ src/component/Lobby/Lobby.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import "./Lobby.css"
+
+class Lobby extends Component {
+    render() {
+        return (
+            <div className="Lobby">
+                <p>[LOBBY]</p>
+            </div>
+        )
+    }
+}
+
+export default Lobby
--- src/page/Game/Game.js
+++ src/page/Game/Game.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react"
 import "./Game.css"
+import Lobby from "../../component/Lobby/Lobby"
+import Chat from "../../component/Chat/Chat"
 
 class Game extends Component {
     render() {
         return (
             <div className="Game">
-                <p>GamePage</p>
+                <Lobby />
+                <Chat />
             </div>
         )
     }
