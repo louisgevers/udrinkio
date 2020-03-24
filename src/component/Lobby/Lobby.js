bfc8a670fd275ef9ab5200085493c1a46bb97933
Louis Gevers <louis.gevers@gmail.com>
2020-03-24 11:05:29 +0100

    Implement Lobby components and style

--- src/component/Lobby/Lobby.js
+++ src/component/Lobby/Lobby.js
@@ -4,8 +4,19 @@ import "./Lobby.css"
 class Lobby extends Component {
     render() {
         return (
-            <div className="Lobby">
-                <p>[LOBBY]</p>
+            <div className="Lobby" style={{backgroundColor: this.props.game.primaryColor}}>
+                <div className="header">
+                    <h1 style={{color: this.props.game.secondaryColor}}>{this.props.game.name}</h1>
+                    <span className="room-code-title">ROOM CODE</span>
+                    <span className="room-code-holder">{this.props.roomId}</span>
+                    <button className="copy-code-button" style={{backgroundColor: this.props.game.secondaryColor}}>copy code</button>
+                </div>
+                <div className="players">
+                    <span className="players-title" style={{color: this.props.game.secondaryColor}}>Players</span>
+                    <span className="players-amount">{`${this.props.game.minPlayers} - ${this.props.game.maxPlayers} players`}</span>
+                    {/* TODO userlist */}
+                </div>
+                <button type="submit" className="start-game-button" style={{backgroundColor: this.props.game.secondaryColor}}>Start game</button>
             </div>
         )
     }
--- src/page/Game/Game.js
+++ src/page/Game/Game.js
@@ -3,11 +3,23 @@ import "./Game.css"
 import Lobby from "../../component/Lobby/Lobby"
 import Chat from "../../component/Chat/Chat"
 
+const placeholderGame = {
+    "id": "pyramid",
+    "name": "PYRAMID",
+    "minPlayers": 2,
+    "maxPlayers": 8,
+    "imageName": "pyramid.png",
+    "primaryColor": "#6D534B",
+    "secondaryColor": "#EEB711"
+}
+
+const placeholderRoomId = 593023
+
 class Game extends Component {
     render() {
         return (
             <div className="Game">
-                <Lobby />
+                <Lobby game={placeholderGame} roomId={placeholderRoomId} />
                 <Chat />
             </div>
         )
