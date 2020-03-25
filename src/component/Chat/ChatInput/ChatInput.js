500357d9cc3cfc86c5853d712ea4abdcd7756ee7
Louis Gevers <louis.gevers@gmail.com>
2020-03-25 14:08:02 +0100

    Implement ChatInput styling

--- src/component/Chat/ChatInput/ChatInput.js
+++ src/component/Chat/ChatInput/ChatInput.js
@@ -5,7 +5,10 @@ class ChatInput extends Component {
     render() {
         return (
             <div className="ChatInput">
-                <span>[CHATINPUT]</span>
+                <textarea className="chat-input" placeholder="Write a message..." maxLength="200" rows="1"/>
+                <button className="chat-send-button">
+                    <span class="material-icons">send</span>
+                </button>
             </div>
         )
     }
