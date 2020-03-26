958f8f6e5a6b6999f84dd2fac899c30cbe5d00c3
Louis Gevers <louis.gevers@gmail.com>
2020-03-26 15:33:42 +0100

    Create MineField divs

--- src/component/GameScreen/MineFieldGame/MineFieldGame.js
+++ src/component/GameScreen/MineFieldGame/MineFieldGame.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
 import './MineFieldGame.css'
 
+import Card from '../Card/Card.js'
+import CardBack from '../CardBack/CardBack.js'
+
 class MineFieldGame extends Component {
+
+    constructor(props) {
+        super(props)
+        // TODO this is used for storybook
+        this.state = this.props.state
+    }
+
     render() {
         return (
             <div className='MineFieldGame game-component'>
-                <span>[MINEFIELDGAME]</span>
+                <div className='card-table'>
+                    {this.state.table.map((row) => {
+                        return <div className='card-row'>
+                            {row.map((cardId) => this.createCard(cardId))}
+                        </div>
+                    })}
+                </div>
             </div>
         )
+    }
+
+    createCard = (cardId) => {
+        if (cardId === 'b') {
+            return <CardBack game={this.state.game} />
+        } else {
+            return <Card cardId={cardId} />
+        }
     }
 }
 
