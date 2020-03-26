bdced15ba537555f2382186b49ebf58b121fef30
Louis Gevers <louis.gevers@gmail.com>
2020-03-26 15:28:00 +0100

    Refactor CardBack for aspect ratio based on height

--- src/component/GameScreen/CardBack/CardBack.js
+++ src/component/GameScreen/CardBack/CardBack.js
@@ -5,15 +5,13 @@ class CardBack extends Component {
     render() {
         return (
             <div className="CardBack">
-                <div className='card-wrapper'>
+                <div className='card-container'>
+                    <img className='size-helper' src={require(`../../../image/cards/1s.svg`)} alt='helper'/>
                     <div className='card-content'>
-                        <div className='card-outline'>
-                            <div className='card-color' style={{backgroundColor: this.props.game.primaryColor}}>
-                                <img src={require(`../../../image/${this.props.game.imageName}`)} alt={`${this.props.cardId}.svg`} />
-                            </div>
+                        <div className='card-color' style={{backgroundColor: this.props.game.primaryColor}}>
+                            <img src={require(`../../../image/${this.props.game.imageName}`)} alt={`${this.props.cardId}.svg`} />
                         </div>
                     </div>
-                    
                 </div>
             </div>
         )
