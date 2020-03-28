818c669105cddfcc1e205e7cacb72e8b81776d39
Louis Gevers <louis.gevers@gmail.com>
2020-03-28 12:42:49 +0100

    Replace MineFieldGame with PIXI.js canvas

--- src/component/GameScreen/MineFieldGame/MineFieldGame.js
+++ src/component/GameScreen/MineFieldGame/MineFieldGame.js
@@ -3,24 +3,14 @@ import './MineFieldGame.css'
 
 import Card from '../Card/Card.js'
 import CardBack from '../CardBack/CardBack.js'
+import * as Pixi from 'pixi.js'
 
 class MineFieldGame extends Component {
 
     constructor(props) {
         super(props)
         this.state = this.props.gameState
-    }
-
-    render() {
-        return (
-            <div className='MineFieldGame game-component'>
-                <div className='card-table' style={{display: 'grid', gridTemplateColumns: `repeat(${this.state.table.length}, 1fr)`}}>
-                    {this.state.table.map((row, i) => {
-                        return row.map((cardId, j) => this.createCard(cardId, i, j))
-                    })}
-                </div>
-            </div>
-        )
+        // this.gameCanvas = React.createRef()
     }
 
     componentWillMount = () => {
@@ -28,27 +18,40 @@ class MineFieldGame extends Component {
         this.props.socket.on('game.userDisconnected', this.onNewGameState)
     }
 
+    componentDidMount = () => {
+        this.app = new Pixi.Application({resizeTo: this.gameCanvas})
+        this.gameCanvas.appendChild(this.app.view)
+        this.app.start()
+    }
+
     componentWillUnmount = () => {
+        this.app.stop()
         this.props.socket.removeListener('minefield.drawnCard', this.onNewGameState)
         this.props.socket.removeListener('game.userDisconnected', this.onNewGameState)
     }
 
-    createCard = (cardId, i, j) => {
-        if (cardId === 'b') {
-            return <CardBack game={this.props.game} onClick={() => this.onCardClick(i, j)} />
-        } else {
-            return <Card cardId={cardId} />
-        }
-    }
-
-    onCardClick = (i, j) => {
-        const data = {row: i, column: j}
-        this.props.socket.emit('minefield.drawCard', data)
+    render() {
+        return (
+            <div ref={(divCanvas) => {this.gameCanvas = divCanvas}} className='MineFieldGame game-component' />
+        )
     }
 
     onNewGameState = (gameState) => {
         this.setState(gameState)
     }
+
+    // createCard = (cardId, i, j) => {
+    //     if (cardId === 'b') {
+    //         return <CardBack game={this.props.game} onClick={() => this.onCardClick(i, j)} />
+    //     } else {
+    //         return <Card cardId={cardId} />
+    //     }
+    // }
+
+    // onCardClick = (i, j) => {
+    //     const data = {row: i, column: j}
+    //     this.props.socket.emit('minefield.drawCard', data)
+    // }
 }
 
 export default MineFieldGame
